69d1e7d5f0a328bc9a51f8dee10f217a905ee472
Mark Benson <mark.benson@nih.gov>
2023-07-14 17:05:59 -0400

    restore - undo test
    putting back to normal api

--- tests/evs_explore.spec.js
+++ tests/evs_explore.spec.js
@@ -4,7 +4,7 @@ test.use({ actionTimeout: 10000 })
 
 test('visit page and take screenshot', async ({ page }) => {
 
-  const response = await page.goto(process.env.ENVIRONMENT_URL || 'https://FAKETEST.evsexplore.nci.nih.gov/evsexplore/welcome')
+  const response = await page.goto(process.env.ENVIRONMENT_URL || 'https://evsexplore.nci.nih.gov/evsexplore/welcome')
 
     // Test that the response did not fail
     expect(response.status()).toBeLessThan(400)
