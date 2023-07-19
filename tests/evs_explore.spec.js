855dd1cacdc6bbb5b554b2bb45a1d5e88da1905f
Mark Benson <mark.benson@nih.gov>
2023-07-19 13:18:00 -0400

    evs explore stage not avail outside NCI firewall

--- tests/evs_explore.spec.js
+++ tests/evs_explore.spec.js
@@ -2,10 +2,14 @@ const { expect, test } = require('@playwright/test')
 
 test.use({ actionTimeout: 10000 })
 
+/* disabling - evs explore Stage is not avail outside of NCI firewall!
 test('visit page and check response code', async ({ page }) => {
 
-  const response = await page.goto('https://evsexplore-stage.nci.nih.gov/evsexplore/welcome')
+  //evsexplore.nci.nih.gov now forwards to evsexplore.csemantics.cancer.gov
+  const response = await page.goto('https://evsexplore-stage.semantics.cancer.gov/evsexplore/welcome')
+  //const response = await page.goto('https://evsexplore-stage.nci.nih.gov/evsexplore/welcome')
 
     // Test that the response did not fail
     expect(response.status()).toBeLessThan(400)
 })
+*/
