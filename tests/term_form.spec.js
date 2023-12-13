fe73ec8ba4e301d79c32a9d1fee2b671177a96bb
Mark Benson <mark.benson@nih.gov>
2023-12-12 22:47:41 -0500

    Update term_form.spec.js - set longer timeout
    test.setTimeout(120000);

--- tests/term_form.spec.js
+++ tests/term_form.spec.js
@@ -4,6 +4,8 @@ test.use({ actionTimeout: 60000 });
 
 test('visit page and take screenshot', async ({ page }) => {
 
+  test.setTimeout(120000);
+  
   const response = await page.goto(process.env.ENVIRONMENT_URL || 'https://ncitermform.nci.nih.gov/ncitermform/?dictionary=NCI%20Thesaurus')
 
     // Test that the response did not fail
