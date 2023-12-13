109fc26a84d58570b64eab761ce9cdd80d53c201
Mark Benson <mark.benson@nih.gov>
2023-12-12 22:47:10 -0500

    Update ncit_hierarchy.spec.js with longer timeout
    Set timeout for a single test

--- tests/ncit_hierarchy.spec.js
+++ tests/ncit_hierarchy.spec.js
@@ -1,6 +1,9 @@
 import { test, expect } from '@playwright/test';
 
 test('PROD Test Erk2 Biochem Pathway in NCIt Hierarchy', async ({ page }) => {
+
+  test.setTimeout(120000);
+  
   await page.goto('https://ncithesaurus.nci.nih.gov/ncitbrowser/pages/hierarchy.jsf?dictionary=NCI_Thesaurus&version=23.02d');
   await page.locator('#IMG_N_4').click();
   await page.locator('#IMG_N_4_9').click();
@@ -14,6 +17,8 @@ test('visit page', async ({ page }) => {
     // to reuse it across your browser checks
     const response = await page.goto(process.env.ENVIRONMENT_URL || 'https://thesaurus.cancer.gov/ncitbrowser/pages/hierarchy.jsf?dictionary=NCI_Thesaurus&version=23.02d');
 
+    test.setTimeout(120000);
+  
     // Test that the response did not fail
     expect(response.status()).toBeLessThan(400);
 
